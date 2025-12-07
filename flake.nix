009b4eb6d0b9ef96356449ab45d6687ac58f7aac
kelwing <kelwing@kelnet.org>
2025-12-07 12:36:36 -0500

    chore: update lockfile

--- flake.nix
+++ flake.nix
@@ -16,65 +16,87 @@
     terraria-server.url = "github:Kelwing/terraria-server";
     flake-utils.url = "github:numtide/flake-utils";
     agenix.url = "github:ryantm/agenix";
-    scibot.url = "https://flakehub.com/f/Kelwing/scibot/0";
+    scibot.url = "github:StarCross-Industries/scibot";
   };
 
   # Flake outputs
   outputs =
-  { self, comin, kelwing-homepage, terraria-server, nixpkgs, flake-utils, agenix, scibot, ... }@inputs:
-  let
-    system = "x86_64-linux";
-    makeNixosSystem =
-    configPath:
-    inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
-      modules = [
-        inputs.determinate.nixosModules.default
-        comin.nixosModules.comin
-        kelwing-homepage.nixosModules.default
-        terraria-server.nixosModules.default
-        scibot.nixosModules.default
-        agenix.nixosModules.default
-        ({...}: {
-          nix.settings = {
-            substituters = ["https://lofn.cachix.org"];
-            trusted-substituters = ["https://lofn.cachix.org"];
-            trusted-public-keys = ["lofn.cachix.org-1:N5d/IDXgSnc4ORB8TpiggU9Wo/VpiWSqrUkE424uBqc="];
-          };
-        })
-        ({...}: {
-          services.comin = {
-            enable = true;
-            remotes = [{
-              name = "origin";
-              url = "https://github.com/Kelwing/nixos-configs.git";
-              branches.main.name = "main";
-            }];
-          };
-        })
-        configPath
-      ];
-    };
-  in
-  {
-    nixosConfigurations.vidar = makeNixosSystem ./hosts/vidar/configuration.nix;
-    # To format all Nix files:
-    # git ls-files -z '*.nix' | xargs -0 -r nix fmt
-    # To check formatting:
-    # git ls-files -z '*.nix' | xargs -0 -r nix develop --command nixfmt --check
-    formatter.${system} = inputs.nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
-  } // flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-  in
-  {
-    devShells.default = pkgs.mkShell {
-      packages = with pkgs; [
-        agenix.packages.${system}.default
-        nixfmt-rfc-style
-      ];
-    };
-  });
+    {
+      self,
+      comin,
+      kelwing-homepage,
+      terraria-server,
+      nixpkgs,
+      flake-utils,
+      agenix,
+      scibot,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+      makeNixosSystem =
+        configPath:
+        inputs.nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          modules = [
+            inputs.determinate.nixosModules.default
+            comin.nixosModules.comin
+            kelwing-homepage.nixosModules.default
+            terraria-server.nixosModules.default
+            scibot.nixosModules.default
+            agenix.nixosModules.default
+            (
+              { ... }:
+              {
+                nix.settings = {
+                  substituters = [ "https://lofn.cachix.org" ];
+                  trusted-substituters = [ "https://lofn.cachix.org" ];
+                  trusted-public-keys = [ "lofn.cachix.org-1:N5d/IDXgSnc4ORB8TpiggU9Wo/VpiWSqrUkE424uBqc=" ];
+                };
+              }
+            )
+            (
+              { ... }:
+              {
+                services.comin = {
+                  enable = true;
+                  remotes = [
+                    {
+                      name = "origin";
+                      url = "https://github.com/Kelwing/nixos-configs.git";
+                      branches.main.name = "main";
+                    }
+                  ];
+                };
+              }
+            )
+            configPath
+          ];
+        };
+    in
+    {
+      nixosConfigurations.vidar = makeNixosSystem ./hosts/vidar/configuration.nix;
+      # To format all Nix files:
+      # git ls-files -z '*.nix' | xargs -0 -r nix fmt
+      # To check formatting:
+      # git ls-files -z '*.nix' | xargs -0 -r nix develop --command nixfmt --check
+      formatter.${system} = inputs.nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            agenix.packages.${system}.default
+            nixfmt-rfc-style
+          ];
+        };
+      }
+    );
 }
