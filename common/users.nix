4cfde3742ce400fcc3ec3da89108dac1af8f6c94
kelwing <kelwing@kelnet.org>
2025-12-07 18:57:14 -0500

    feat: add pubkey for github deploys

--- common/users.nix
+++ common/users.nix
@@ -8,6 +8,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFjnOwCitFWrv7GeMnqIsuRAj/oA2B8ehrtqjCkFnqBg kelwing@desktop"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgW0mNhzaFegPBdf35nTks7bZVJQLMYh5ZgpA1V2GGb kelwing@mbp"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEEBIF1VgcvTRqgnEhrJYxrT0zy9zdWNkYjnpn9nJn2c github-deploy"
       ];
     };
   };
