be0e792caba281a53dd29041f0c86415d6cd5c24
Kelwing <kelwing@kelnet.org>
2026-03-22 21:39:32 -0400

    fix: remove duplicate settings

--- hosts/vidar/configuration.nix
+++ hosts/vidar/configuration.nix
@@ -7,15 +7,6 @@
   ];
 
   nixpkgs.config.allowUnfree = true;
-  nix = {
-    settings = {
-      experimental-features = "nix-command flakes";
-      trusted-users = [
-        "root"
-        "@wheel"
-      ];
-    };
-  };
 
   boot = {
     loader = {
