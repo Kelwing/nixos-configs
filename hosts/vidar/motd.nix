70c9a9ed1f92c417c1e726467e0b39b4cb99e9d9
kelwing <kelwing@kelnet.org>
2026-04-06 17:12:11 -0400

    fix: move figlet to service config, add bash and coreutils

--- hosts/vidar/motd.nix
+++ hosts/vidar/motd.nix
@@ -1,32 +1,26 @@
 { pkgs, ... }:
 {
-  environment = {
-    systemPackages = with pkgs; [
-      rust-motd
-      figlet
-    ];
-    etc."motd.kdl".text = ''
-      global {
-        version "1.0"
-      }
+  environment.etc."motd.kdl".text = ''
+    global {
+      version "1.0"
+    }
 
-      components {
-        command "hostname | figlet -f slant"
-        filesystems {
-          filesystem name="root" mount-point="/"
-        }
-        service-status {
-          service display-name="SCI Bot" unit="scibot"
-          service display-name="Terraria" unit="terraria"
-          service display-name="Tuwunel" unit="tuwunel"
-          service display-name="Coturn" unit="coturn"
-          service display-name="Impostor" unit="impostor"
-        }
-        uptime
-        load-avg format="Load (1, 5, 15 min.): {one:.02}, {five:.02}, {fifteen:.02}"
+    components {
+      command "hostname | figlet -f slant"
+      filesystems {
+        filesystem name="root" mount-point="/"
       }
-    '';
-  };
+      service-status {
+        service display-name="SCI Bot" unit="scibot"
+        service display-name="Terraria" unit="terraria"
+        service display-name="Tuwunel" unit="tuwunel"
+        service display-name="Coturn" unit="coturn"
+        service display-name="Impostor" unit="impostor"
+      }
+      uptime
+      load-avg format="Load (1, 5, 15 min.): {one:.02}, {five:.02}, {fifteen:.02}"
+    }
+  '';
 
   systemd = {
     timers."rust-motd" = {
@@ -38,6 +32,11 @@
       };
     };
     services."rust-motd" = {
+      path = [
+        pkgs.bash
+        pkgs.coreutils
+        pkgs.figlet
+      ];
       script = ''
         set -eu
         ${pkgs.rust-motd}/bin/rust-motd /etc/motd.kdl > /etc/motd
